bca973f44fe98a82b9dc0f2e82210c25b1e051f8
qlyu001 <qlyu001@ucr.edu>
2017-07-18 23:43:22 -0700

    add more test

--- request.py
+++ request.py
@@ -1,4 +1,5 @@
 import requests, time
+from requests import Session
 r = requests.post('https://requestb.in/1lpgxkz1', data={"ts":time.time()})
 print (r.status_code)
 print (r.content)
@@ -10,3 +11,26 @@ print (s.post('http://httpbin.org/post',{'key':'value'},headers={'user-agent':'N
 print (s.get('http://httpbin.org/status/404').status_code)
 print (s.get('http://httpbin.org/html').text)
 print (s.get('http://httpbin.org/deny').text)
+
+
+
+session = Session()
+
+# HEAD requests ask for *just* the headers, which is all you need to grab the
+# session cookie
+session.head('http://sportsbeta.ladbrokes.com/football')
+
+response = session.post(
+                        url='http://sportsbeta.ladbrokes.com/view/EventDetailPageComponentController',
+                        data={
+                        'N': '4294966750',
+                        'form-trigger': 'moreId',
+                        'moreId': '156#327',
+                        'pageType': 'EventClass'
+                        },
+                        headers={
+                        'Referer': 'http://sportsbeta.ladbrokes.com/football'
+                        }
+                        )
+
+print (response.text)
