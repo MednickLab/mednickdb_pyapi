e26d8151b564226c2ec2764e67105a52f61eb24f
bdyetton@gmail.com <bdyetton@gmail.com>
2019-01-15 23:57:44 -0800

    upload helper complete and all tests passing

--- mednickdb_pyapi/upload_helper.py
+++ mednickdb_pyapi/upload_helper.py
@@ -1,8 +1,8 @@
 import sys
 import glob
 import re
-import os
-#subject_stems = {'subjectid', 'subjectnum', 'subject', 'subj', 'subject'}
+from typing import List
+
 re_i_type = '\d+'
 re_s_type = '[a-z0-9]*'
 
@@ -16,18 +16,20 @@ default_pattern_keys = {
     'fileformat': re_s_type
 }
 
-type_map = {'i':'\d+',
-            's':'[A-z0-9]*'}
+type_map = {'int':'\d+',
+            'str':'[A-z0-9]*'}
 
 
-def parse_args_to_reg_ex(search_params):
+def _parse_args_to_reg_ex(search_pattern: str):
     """
-    TODO
-    :param search_params:
+    Take a human readable string pattern and parse to a re expression, and a set of keys and types to extract from a filename
+    :param search_pattern: A string to match files to, where specifiers, like subjectid, visitid, etc can be extracted by adding {subjectid}.
+        e.g. if a filename is mydata_subid43, then mydata_subid{subjectid} will create a regular expression to
+        match mydata_subid and also extract 43 as the subjectid.
     :return:
     """
     # parse filename matching
-    pattern_to_match = search_params#sys.argv[2]
+    pattern_to_match = search_pattern#sys.argv[2]
     pattern_components = pattern_to_match.split('}')[:-1]
     pattern_stems = [pattern.split('{')[0] for pattern in pattern_components]
     pattern_key_parts = [pattern.split('{')[1] for pattern in pattern_components]
@@ -49,14 +51,15 @@ def parse_args_to_reg_ex(search_params):
     return re_exp, pattern_keys, pattern_types
 
 
-def file_path_to_file_info(file_path, re_exp, pattern_keys, pattern_types):
+def _file_path_to_upload_info(file_path: str, re_exp: str, pattern_keys: List[str], pattern_types: List[str]):
     """
-    TODO
-    :param file_path:
-    :param re_exp:
-    :param pattern_keys:
-    :param pattern_types:
-    :return:
+    To be used in conjunction with _parse_args_to_reg_ex.
+    Parsing can automatically remove parts of the filename and set as any
+    :param file_path: file path to parse.
+    :param re_exp: regular expression string to match
+    :param pattern_keys: names of the keys, in order for the created file_info object
+    :param pattern_types: types for the values to be extracted from
+    :return: upload_info, a dict of values extracted from file_path with types pattern_types, and keys pattern_keys
     """
     values = re.search(re_exp, file_path, re.IGNORECASE)
     file_info = {}
@@ -70,22 +73,82 @@ def file_path_to_file_info(file_path, re_exp, pattern_keys, pattern_types):
     return file_info
 
 
-if __name__ == '__main__':
-    folder_to_search = sys.argv[1]
+def run_upload_helper(folder_to_search: str, pattern: str, default_upload_args: str=None):
+    """
+    Searches through a folder and uploads file that match pattern. Can extract upload dict key:value pairs from filenames.
+    :param folder_to_search: abs or regular path to folder to search through e.g. 'C:/Users/bdyet/data/'
+    :param pattern: only upload files that match this pattern. Key/Valye pairs to add to upload dict can be added with {keyname},
+        e.g. if the file name is eeg_subid32.edf, then subid{subjectid} will match that file, and also add subjectid=32 to the upload dict.
+        types (either int or str) of the values will be inferred if known (e.g. subjectid->int, studyid->str) or can be set subid{subjectid=str}
+    :param default_upload_args: common key/value pairs to add to all the matched files upload dic, as a string.
+        e.g. 'studyid=PSTIM otherkey=somevalue'
+    :return: a list of sucessfully uploaded files
+    """
+
+    files_ready_to_upload, n_files = _gather_files_to_upload(folder_to_search, pattern, default_upload_args)
+    files_to_actually_upload = []
+
+    print('%i files in folder, %i match pattern and are ready for upload' % (n_files, len(files_ready_to_upload)))
+    print('Files are:')
+    for idx, file_info in enumerate(files_ready_to_upload):
+        print(idx, ':', file_info['filepath'])
+        for key, value in file_info.items():
+            if key is not 'filepath':
+                print('   ', key, '=', value)
+        ans = input('Is this correct? (y-yes, n-no, a-yes to all, q-quit)?')
+        if ans == 'a':
+            print('--uploading all files')
+            return None
+        elif ans == 'y':
+            files_to_actually_upload.append(file_info)
+            print('--upload this file')
+            continue
+        elif ans == 'n':
+            print('--skipped this file')
+            continue
+        else:
+            print('Quitting, no files uploaded')
+            return None
+
+    # upload TODO
+    return files_to_actually_upload
+
+
+def _gather_files_to_upload(folder_to_search: str, pattern: str, default_upload_args: str=None):
+    """
+    Searches through a folder and uploads file that match pattern. Can extract upload dict key:value pairs from filenames.
+    :param folder_to_search: abs or regular path to folder to search through e.g. 'C:/Users/bdyet/data/'
+    :param pattern: only upload files that match this pattern. Key/Valye pairs to add to upload dict can be added with {keyname},
+        e.g. if the file name is eeg_subid32.edf, then subid{subjectid} will match that file, and also add subjectid=32 to the upload dict
+    :param default_upload_args: common key/value pairs to add to all the matched files upload dic, as a string.
+        e.g. 'studyid=PSTIM otherkey=somevalue'
+    :return: a list of upload dicts for the files to upload. File path will be added as a "filename" key to each.
+    """
+
     if folder_to_search[-1] is not '/' or folder_to_search[-1] is not '\\':
         folder_to_search += '/'
 
     # parse filename matching
-    re_exp, pattern_keys, pattern_types = parse_args_to_reg_ex(sys.argv[2])
+    re_exp, pattern_keys, pattern_types = _parse_args_to_reg_ex(pattern)
 
     # parse specifiers for the whole folder
-    if len(sys.argv) > 3:
+    if default_upload_args is not None:
         other_specifiers = sys.argv[3:]
         files_info = {other_spec.split('=')[0]: other_spec.split('=')[1] for other_spec in other_specifiers}
     else:
         files_info = {}
 
-    for file_path in glob.iglob(folder_to_search + '*'):
-        file_info = file_path_to_file_info(file_path, re_exp, pattern_keys, pattern_types)
-        file_info = file_info.update(files_info)
-        print(file_path, '-->', file_info)
+    file_infos = []
+    files_in_folder = glob.glob(folder_to_search + '*')
+    for file_path in files_in_folder:
+        file_info = _file_path_to_upload_info(file_path, re_exp, pattern_keys, pattern_types)
+        file_info.update(files_info)
+        file_info['filepath'] = file_path
+        file_infos.append(file_info)
+
+    return file_infos, len(files_in_folder)
+
+
+if __name__ == '__main__':
+    run_upload_helper(sys.argv[1], sys.argv[2], sys.argv[3:] if len(sys.argv)>2 else None)
+
